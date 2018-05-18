8d2c6b5febbf3fca3c4fb12addc7cf7e8f62b511
yuanyican <max.yuan.ivan@gmail.com>
2018-05-18 13:14:58 +0800

    update

---
+++ pages/guide/guide.js
@@ -0,0 +1,142 @@
+// pages/guide/guide.js
+
+// 引入SDK核心类
+let amapFile = require('../../libs/amap-wx.js');
+let QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+let config = require('../../utils/config.js');
+
+let qqKey = config.Config.qqKey;
+let aKey = config.Config.aKey;
+// 实例化API核心类
+let qqMapWX = new QQMapWX({ key: qqKey });
+let amapFun = new amapFile.AMapWX({ key: aKey });
+
+Page({
+  data: {
+    ampFunc: null,
+    lStart: {},
+    lEnd: {},
+    scale: '12',
+    includePoints: [],
+    markers: [],
+    distance: '',
+    cost: '',
+    polyline: [],
+    origin: '',
+    destination: '',
+    isShowMsg: false
+  },
+
+  chooseLocation (e) {
+    wx.chooseLocation({
+      success: res => {
+        let data = {}
+        let lType = e.currentTarget.dataset.ltype
+        let temp = lType === 'lStart' ? 'origin' : 'destination'
+        data[lType] = res
+        data[temp] = `${res.longitude},${res.latitude}`
+        this.setData(data)
+        if(this.data.lStart && this.data.lEnd) this.do()
+      },
+    })
+  },
+
+  onLoad: function () {
+
+    wx.getLocation({
+      success: res => {
+        qqMapWX.reverseGeocoder({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: res => {
+            let start = {
+              address: res.result.address,
+              name: res.result.formatted_addresses.recommend,
+              latitude: res.result.location.lat,
+              longitude: res.result.location.lng
+            }
+            this.setData({
+              lStart: start,
+              origin: `${start.longitude},${start.latitude}`
+            })
+          },
+          fail: function (res) {
+            // console.log(res);
+          },
+          complete: function (res) {
+            // console.log(res);
+          }
+        });
+      }
+    })
+  },
+
+  exchangeLocation () {
+    let start = this.data.lStart
+    let end = this.data.lEnd
+    this.setData({
+      lStart: end,
+      lEnd: start
+    })
+  },
+
+  do (route) {
+    let routeType = route || 'getDrivingRoute'
+    amapFun[routeType]({
+      origin: this.data.origin,
+      destination: this.data.destination,
+      success: data => {
+        let points = [];
+        if (data.paths && data.paths[0] && data.paths[0].steps) {
+          let steps = data.paths[0].steps;
+          for (let i = 0; i < steps.length; i++) {
+            let poLen = steps[i].polyline.split(';');
+            for (let j = 0; j < poLen.length; j++) {
+              points.push({
+                longitude: parseFloat(poLen[j].split(',')[0]),
+                latitude: parseFloat(poLen[j].split(',')[1])
+              })
+            }
+          }
+        }
+        this.setData({
+          polyline: [{
+            points: points,
+            color: "#0091ff",
+            width: 6
+          }],
+          includePoints: points,
+        });
+        if (data.paths[0] && data.paths[0].distance) {
+          this.setData({
+            distance: data.paths[0].distance + '米'
+          });
+        }
+        if (data.taxi_cost) {
+          this.setData({
+            cost: '打车约' + parseInt(data.taxi_cost) + '元'
+          });
+        }
+      },
+      fail: function (info) {
+
+      }
+    })
+  },
+  goToCar: function () {
+    this.do()
+  },
+  goToBus: function (e) {
+    wx.redirectTo({
+      url: '../navigation_bus/navigation'
+    })
+  },
+  goToRide: function (e) {
+    this.do('getRidingRoute')
+  },
+  goToWalk: function (e) {
+    this.do('getWalkingRoute')
+  }
+})
---
+++ utils/config.js
@@ -0,0 +1,6 @@
+const config = {
+  aKey: 'e92713ad69cdbd2779e4f5e3e4b4a589',
+  qqKey: 'QF2BZ-BFP2K-KJOJM-AJRYR-7XIVH-COFY4'
+}
+
+module.exports.Config = config;
